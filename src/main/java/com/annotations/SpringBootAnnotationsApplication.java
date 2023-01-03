24a0d6ae95f936c8b5d448b581e54f01737b70f1
Praveen V Tavare <praveentavare@gmail.com>
2023-01-03 15:45:59 +0530

    Changes in main() method

--- src/main/java/com/annotations/SpringBootAnnotationsApplication.java
+++ src/main/java/com/annotations/SpringBootAnnotationsApplication.java
@@ -1,13 +1,48 @@
 package com.annotations;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.util.Date;
 
-@SpringBootApplication
-public class SpringBootAnnotationsApplication 	{
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+import com.mypack.Dog;
+
+//@SpringBootApplication
+// @SpringBootApplication Equivalent to @EnableAutoConfiguration, @Configuration and @ComponentScan
+
+@EnableAutoConfiguration
+@ComponentScan
+@Configuration
+public class SpringBootAnnotationsApplication implements CommandLineRunner {
+
+	@Autowired
+//	@Qualifier("student1")
+	@Qualifier("student2")
+	private Student student;
+
+	@Autowired
+	private Date date;
+
+	@Autowired
+	private Emp emp;
+
+	@Autowired
+	private Dog dog;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootAnnotationsApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		this.student.studying();
+		this.emp.yourName();
+		this.dog.eating();
+	}
+ 
 }
